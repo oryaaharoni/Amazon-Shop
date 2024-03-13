d1a09455ca6eeea8f5b0e085ffb80544e9a11deb
oryaaharoni <oryaaharonii@gmail.com>
2024-03-13 13:31:11 +0200

    add inDevOrProd variable

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { HelmetProvider} from 'react-helmet-async'
-import "bootstrap/dist/css/bootstrap.min.css"
-import './index.css'
-import  axios  from 'axios'
-import { StoreProvider } from './Store.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { HelmetProvider } from "react-helmet-async";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import axios from "axios";
+import { StoreProvider } from "./Store.jsx";
 
-
-axios.defaults.baseURL = import.meta.env.DEV ? 'http://localhost:8080' : 'https://amazon-shop-ten.vercel.app'; 
-console.log(import.meta.env.DEV);
-ReactDOM.createRoot(document.getElementById('root')).render(
+const inDevOrProd = import.meta.env.DEV;
+axios.defaults.baseURL = inDevOrProd
+  ? "http://localhost:8080"
+  : "https://amazon-shop-ten.vercel.app";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <StoreProvider>
       <HelmetProvider>
         <App />
       </HelmetProvider>
     </StoreProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
